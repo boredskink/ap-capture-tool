1a49bca6db26854f66e879f663739452303603de
BoredSkink <96706643+boredskink@users.noreply.github.com>
2024-09-01 22:21:51 +1000

    tidied template layout

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__, template_folder="static/templates")
+app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")

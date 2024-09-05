25e46e21e7caaa578e512cae05380b584070f8f3
Justin Grigoleit <101234294+justingap@users.noreply.github.com>
2024-09-05 17:51:30 +1000

    improved labels

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect
+from flask_restful import Api
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
+api = Api(app)
 
-class CapturePayment(FlaskForm):
-        ordernumber = StringField('ordernumber', validators=[DataRequired()])
+class CapturePaymentForm(FlaskForm):
+        ordernumber = StringField('Order Number', validators=[DataRequired()])
 
 @app.route("/")
 @app.route("/index", methods=('GET', 'POST'))
 def index():
-        form = CapturePayment(meta={'csrf': False})
+        form = CapturePaymentForm(meta={'csrf': False})
         return render_template("index.html", form=form)
